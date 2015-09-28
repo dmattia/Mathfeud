ac715f16f6411d82e5e626d94d3ac066c7733341
David Mattia <dmattia@nd.edu>
2015-09-28 00:29:01 -0400

    Added new views to blog

--- main/models.py
+++ main/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
 # Create your models here.
-class GroupProfile(models.Model)
-	group = models.ForeignKey(Group, unique=True)
+class GroupProfile(models.Model):
 	school = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name
+	@classmethod
+	def create(self, name, school):
+		obj = GroupProfile.objects.create(name=name, school=school)
+		obj.save()
+		return obj
 
-class UserProfile(models.Model)
-	user = models.ForeignKey(User, unique=True)
-	group = models.ForeignKey(GroupProfile, unique=True)
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, primary_key=True)
+	group = models.ForeignKey(GroupProfile)
+	def __unicode__(self):
+		return self.user.username
+
+admin.site.register(GroupProfile)
+admin.site.register(UserProfile)
--- main/regbackend.py
+++ main/regbackend.py
@@ -1,14 +1,32 @@
 from registration.backends.default.views import RegistrationView
 from forms import UserProfileRegistrationForm
-from models import UserProfile
+from models import UserProfile, GroupProfile
+from django.contrib.auth.models import Group
+from registration.forms import RegistrationFormUniqueEmail
 
 class MyRegistrationView(RegistrationView):
 	form_class = UserProfileRegistrationForm
 	
 	def register(self, request, form_class):
 		new_user = super(MyRegistrationView, self).register(request, form_class)
-		new_group = 
+		g = Group.objects.get(name='groupAdmin')
+		g.user_set.add(new_user)
+		group_profile = GroupProfile.create(name=form_class.cleaned_data['group_name'], school=form_class.cleaned_data['school'])
 		user_profile = UserProfile()
 		user_profile.user = new_user
-		user_profile.school = form_class.cleaned_data['school']
+		user_profile.group = group_profile
+		user_profile.save()
+
+class GroupMemberRegView(RegistrationView):
+	form_class = RegistrationFormUniqueEmail
+
+	def register(self, request, form_class):
+		new_user = super(GroupMemberRegView, self).register(request, form_class)
+		g = Group.objects.get(name='groupMember')
+		g.user_set.add(new_user)
+		group_name = request.GET.get('group_name')
+		group_profile = GroupProfile.objects.get(name=group_name)
+		user_profile = UserProfile()
+		user_profile.user = new_user
+		user_profile.group = group_profile
 		user_profile.save()
