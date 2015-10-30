bd13c1783de896950325161e04f175232883808e
David Mattia <dmattia@nd.edu>
2015-10-30 14:32:54 -0400

    Fixed broken link to individual videos

--- video/urls.py
+++ video/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from video import views
 
 urlpatterns = [ 
-	url(r'topics/$', views.topicList, name='topics'),
-        url(r'topics/(.+)/$', views.getVideos, name='videos'),
-        url(r'(\d+)/$', views.getVideo, name='video'),
+	url(r'^$', views.topicList, name='topics'),
+        url(r'^([^/]+)/$', views.getVideos, name='videos'),
+        url(r'^.+/(\d+)/$', views.getVideo, name='video'),
 ]
