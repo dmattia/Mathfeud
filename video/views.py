a17cf5b7ed4c8fd5a613f145101ed492a578c0e4
David Mattia <dmattia@nd.edu>
2015-10-30 14:30:30 -0400

    Added topics page for videos

--- video/views.py
+++ video/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, render_to_response
 from video.models import *
 # Create your views here.
 
-def getVideos(request):
-        return render(request, 'main/videos.html', {'videos': Video.objects.order_by('grade')})
+def topicList(request):
+	return render(request, 'main/topics.html', {'topics': Topic.objects.all().order_by('grade')})
+
+def getVideos(request, topicName):
+        return render(request, 'main/videos.html', {'videos': Video.objects.filter(topic=Topic.objects.get(name=topicName))})
 
 def getVideo(request, vidNumber):
         return render(request, 'main/video.html', {'video': Video.objects.get(id=vidNumber)})
