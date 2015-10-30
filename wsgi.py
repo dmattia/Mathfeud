8dd0aa7d6f1f9e4aa4afab6dc60341821d5d4631
David Mattia <dmattia@nd.edu>
2015-10-30 14:30:30 -0400

    Added topics page for videos

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import site
+
+sys.path.append('/mathfeud/mathfeud')
+sys.path.append('/mathfeud/mathfeud/mathfeud')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mathfeud.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mathfeud.settings")
